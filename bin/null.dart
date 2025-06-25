49c6c151ecfd25f8a731934a3542993e42aca6db
yusuf <yusufnew060@gmail.com>
2025-06-25 14:15:50 +0700

    Add initial Dart code demonstrating nullable variables

---
+++ bin/null.dart
@@ -0,0 +1,8 @@
+void main() {
+  // int number1;
+  int? number2;
+
+  // print(number1); // This will cause an error because number1 is not initialized
+  print(
+      number2); // This will print null because number2 is declared but not initialized
+}
