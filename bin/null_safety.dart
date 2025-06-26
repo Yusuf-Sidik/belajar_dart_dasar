75148b777fe25a24ee17fde11efbb161502a66ae
yusuf <yusufnew060@gmail.com>
2025-06-26 09:12:00 +0700

    Add conversion examples for nullable and non-nullable types in null safety demonstration

--- bin/null_safety.dart
+++ bin/null_safety.dart
@@ -13,4 +13,29 @@ void main() {
   } else {
     print('Age is null, cannot convert to double.');
   }
+
+  // conversion null
+  // contoh conversi dari non-nullable ke nullable
+  print('\nconversion null');
+  String name = 'Yusuf';
+  String? nullableName = name;
+  print(nullableName);
+
+  // contoh conversi dari nullable ke non-nullable
+  int? nullableNumber;
+  // ignore: unnecessary_null_comparison
+  if (nullableNumber != null) {
+    int number =
+        nullableNumber; // Jika nullableName tidak null, kita bisa menggunakannya sebagai non-nullable
+    print(number);
+  } else {
+    print('nullableNumber is null, cannot convert to non-nullable int.');
+  }
+
+  // contoh default value
+  print('\ncontoh default value');
+  String? guest;
+  var guestName =
+      guest ?? 'Guest'; // Jika guest null, gunakan 'Guest' sebagai default
+  print(guestName); // Output: Guest
 }
