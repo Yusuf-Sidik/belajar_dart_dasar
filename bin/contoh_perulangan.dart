63f03924e832697607db348fb78f0e10d56e3230
yusuf <yusufnew060@gmail.com>
2025-06-28 14:02:16 +0700

    Refactor main function to remove duplicate days array declaration and improve code structure

--- bin/contoh_perulangan.dart
+++ bin/contoh_perulangan.dart
@@ -1,9 +1,17 @@
 void main() {
+  var days = ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jum\'at', 'Sabtu', 'Minggu'];
+  var counter = 1;
   // contoh perulangan for
   print('contoh perulangan for');
-  var days = ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jum\'at', 'Sabtu', 'Minggu'];
 
   for (int i = 0; i < days.length; i++) {
     print('indeks hari ke- $i hari ${days[i]}');
+  }
+
+  // contoh perulangan while
+  print('\ncontoh perulangan while');
+  while (counter <= 10) {
+    print('Perulangan ke-$counter');
+    counter++;
   }
 }
