d718f0e0aaffb604ce45281e945d1107c5d0fecc
yusuf <yusufnew060@gmail.com>
2025-06-28 13:34:32 +0700

    Refactor for loop demonstration by adding infinite loop example and enhancing comments

--- bin/for.dart
+++ bin/for.dart
@@ -1,9 +1,23 @@
 void main() {
+  var counter = 1;
   /*
   perulangan tanpa init statement, kondisi, post statement
   akan menjalankan bloc perulangan tanpa henti
-  */
+  contoh :
   for (;;) {
     print('Perulangan Tanpa Henti');
+  }
+
+  perulangan dengan menggunakan kondisi agar suatu saat kondisinya false.
+  setelah kondisi menjadi false, perulangan akan berhenti.
+  contoh:
+  for (; counter <= 10;) {
+    print('Perulangan ke_$counter');
+    counter++;
+  }
+  */
+  for (; counter <= 10;) {
+    print('Perulangan ke-$counter');
+    counter++;
   }
 }
