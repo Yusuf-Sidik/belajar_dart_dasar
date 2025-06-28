a640d4d93158630c8922e1092545fef94b613149
yusuf <yusufnew060@gmail.com>
2025-06-28 13:29:34 +0700

    Add infinite loop example in for loop demonstration

--- bin/for.dart
+++ bin/for.dart
@@ -3,7 +3,7 @@ void main() {
   perulangan tanpa init statement, kondisi, post statement
   akan menjalankan bloc perulangan tanpa henti
   */
-  // for (;;) {
-  //   print('Perulangan Tanpa Henti');
-  // }
+  for (;;) {
+    print('Perulangan Tanpa Henti');
+  }
 }
