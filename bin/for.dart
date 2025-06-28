87370b4557db7e6579ce81563edb2b4ece9a3574
yusuf <yusufnew060@gmail.com>
2025-06-28 13:26:39 +0700

    Add infinite loop example in main function

---
+++ bin/for.dart
@@ -0,0 +1,9 @@
+void main() {
+  /*
+  perulangan tanpa init statement, kondisi, post statement
+  akan menjalankan bloc perulangan tanpa henti
+  */
+  // for (;;) {
+  //   print('Perulangan Tanpa Henti');
+  // }
+}
