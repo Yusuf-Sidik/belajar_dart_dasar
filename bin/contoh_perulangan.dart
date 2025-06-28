51b05ef73794767e9b529ceaeb3d9e76cb7d8c05
yusuf <yusufnew060@gmail.com>
2025-06-28 13:54:13 +0700

    Add initial for loop example in Dart

---
+++ bin/contoh_perulangan.dart
@@ -0,0 +1,9 @@
+void main() {
+  // contoh perulangan for
+  print('contoh perulangan for');
+  var days = ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jum\'at', 'Sabtu', 'Minggu'];
+
+  for (int i = 0; i < days.length; i++) {
+    print('indeks hari ke- $i hari ${days[i]}');
+  }
+}
